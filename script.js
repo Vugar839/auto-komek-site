b329c44526d926df07dc9004c734924ee0fb18be
Vugar839 <usubovvugar2@gmail.com>
2025-05-20 22:17:33 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-document.getElementById('contactForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('Спасибо за сообщение!');
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    // Получаем значения
+    const name = this.name.value.trim();
+    const email = this.email.value.trim();
+    const message = this.message.value.trim();
+    const formMessage = document.getElementById('formMessage');
+
+    if(name && email && message) {
+        formMessage.style.color = "green";
+        formMessage.textContent = "Спасибо за ваше сообщение, мы свяжемся с вами в ближайшее время.";
+        this.reset();
+    } else {
+        formMessage.style.color = "red";
+        formMessage.textContent = "Пожалуйста, заполните все поля.";
+    }
 });
